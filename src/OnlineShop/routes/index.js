f6ca2590e2c40b151b975f0988f3dcb31680ca11
DaoBinhMinh <binhm63@gmail.com>
2018-03-27 01:22:09 +0700

    Shopping Cart

--- src/OnlineShop/routes/index.js
+++ src/OnlineShop/routes/index.js
@@ -34,49 +34,4 @@ router.get('/detail/:id', function (req, res) {
         }
     })
 })
-//trc t mới chuyển qua đ biết có bỏ vô chưa nữa ~
-// router.get('/admin', isAuthenticated, function (req, res) {
-//   res.status(200).json({
-//     status: 'Login successful!' + req.user
-//   });
-// })
-
-// function isAuthenticated(req, res, next) {
-//   if (req.user) {
-//     DB.checkRole(req.user, function (err) {
-//       if (err) {
-//         return res.status(401).json({
-//           error: 'Khong phai admin ' + req.user
-//         })
-//       } else {
-//         return next();
-//       }
-//     })
-//   } else {
-//     return res.status(401).json({
-//       error: 'Chua Dang Nhap'
-//     })
-//   }
-// }
-// function check(username) {
-//   temp = false;
-//   DB.checkRole(username, function (err, callback) {
-//     if (err) {
-//       console.log('ko phai')
-//       temp = false;
-//     } else {
-//       console.log('Admin day ne')
-//       temp = true;
-//     }
-//   })
-//   return temp;
-// }
-/*
-router.get('/private', function (req, res) {
-  if (req.isAuthenticated() ) {
-    res.send('This is private page' + req.session.user)
-  } else {
-    res.send('Chua Dang Nhap');
-  }
-})*/
 module.exports = router;
